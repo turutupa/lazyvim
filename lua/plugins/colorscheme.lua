a8330c354f98e1296c790ae139e5fb54b75b441d
alberto.d <vn562er@email.wal-mart.com>
2024-05-06 11:15:02 -0700

    Kanagawa colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -44,7 +44,6 @@ return {
 
   {
     "folke/tokyonight.nvim",
-
     opts = {
       transparent = true,
       styles = {
@@ -55,11 +54,46 @@ return {
   },
 
   { "embark-theme/vim", as = "embark" },
-  --  set colorscheme
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = false,
+    config = function()
+      require("kanagawa").setup({
+        compile = true,
+        functionStyle = { bold = true },
+        dimInactive = false,
+        colors = {
+          theme = {
+            all = {
+              ui = {
+                bg_gutter = "none",
+              },
+            },
+          },
+        },
+      })
+      vim.cmd("colorscheme kanagawa-wave")
+    end,
+    overrides = function(colors)
+      local theme = colors.theme
+      return {
+        NormalFloat = { bg = "none" },
+        FloatBorder = { bg = "none" },
+        FloatTitle = { bg = "none" },
+        NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+        LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+        MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+        Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 },
+        PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+        PmenuSbar = { bg = theme.ui.bg_m1 },
+        PmenuThumb = { bg = theme.ui.bg_p2 },
+      }
+    end,
+  },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight-moon",
+      colorscheme = "kanagawa",
     },
   },
 }
